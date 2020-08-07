935db08236cc11dd96fb816af3d70c78c7bcf615
Steven Acevedo <astevenaa@gmail.com>
2020-08-07 02:11:50 -0700

    added a connection to user to a artist collection that havnt be created yet

--- models/user.js
+++ models/user.js
@@ -3,8 +3,14 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   artist: [{ firstName: String, lastName: String }],
-  username: {type: String, required: true},
-  password: {type: String, required: true}
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  cart: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "artist",
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
