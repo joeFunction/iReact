1797b8a5f65cf8ea9b7fcf4248a6890425be9259
Joe Adams <adams.jd10@gmail.com>
2020-08-08 12:42:32 -0700

    update

--- models/user.js
+++ models/user.js
@@ -2,9 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  artist: [{ firstName: String, lastName: String }],
-  username: {type: String, required: true},
-  password: {type: String, required: true}
+  artist: {type: String, required: true},
+  id: {type: String, required: true}
 });
 
 const User = mongoose.model("User", userSchema);
