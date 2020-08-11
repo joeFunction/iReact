24c2e68fee004d9856df028bec0c9cfcb19237b3
esme rodriguez <rodriguezesme24@gmail.com>
2020-08-10 22:37:17 -0700

    Fixed AuthO

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -78,63 +78,6 @@ let Books = () => {
   let [books, setBooks] = useState([])
   let [title, setTitle] = useState('')
 
-// import { Col, Row, Container } from "../components/Grid";
-// import { List, ListItem } from "../components/List";
-// import { Input, TextArea, FormBtn } from "../components/Form";
-
-function Books() {
-  // Setting our component's initial state
-  const [books, setBooks] = useState([])
-  const [formObject, setFormObject] = useState({})
-
-
-  // Load all books and store them with setBooks
-  useEffect(() => {
-    loadBooks()
-  }, [])
-
-  // Loads all books and sets them to books
-  function loadBooks() {
-    API.getBooks()
-      .then(res => 
-        setBooks({books: res.data.data})
-      )
-      .catch(err => console.log(err));
-  };
-
-  //   //deezer
-  // loadBooks = async (res) => {
-  //   try {
-  //     console.log(res.data.data)
-  //     this.setState({ books: res.data.data });
-
-  // Deletes a book from the database with a given id, then reloads books from the db
-  function deleteBook(id) {
-    API.deleteBook(id)
-      .then(res => loadBooks())
-      .catch(err => console.log(err));
-  }
-
-  // Handles updating component state when the user types into the input field
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-  };
-
-  // When the form is submitted, use the API.saveBook method to save the book data
-  // Then reload books from the database
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis
-      })
-        .then(res => loadBooks())
-        .catch(err => console.log(err));
-    }
-  };
 
 
   return (
