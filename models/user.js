a245c3b2a6957d32643443b8900fcf531489d312
Steven Acevedo <astevenaa@gmail.com>
2020-08-11 21:08:44 -0700

    make your own branch

--- models/user.js
+++ models/user.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  artist: {type: String, required: true},
-  id: {type: String, required: true}
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  favorites: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "artist",
+    },
+  ],
 });
 
 const User = mongoose.model("User", userSchema);
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("express-promise-router")();
 
-const UserController = require("../../controllers/userController");
+const UserController = require("../../controllers/userArtistController");
 router
   .route("/:userId/artist")
   .get(UserController.getUserArtist)
